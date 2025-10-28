7c7ea732745caa5005145a6fe9cd4e45c1839dd6
Sze Chuan <szechuan.lai@illumina.com>
2025-10-29 07:21:24 +0800

    Organize aliases into separate files
    - Create ~/.config/zsh/aliases.zsh for general aliases
    - Create ~/.config/zsh/work-aliases.zsh for work-specific aliases (MacOS only)
    - Update .zshrc to source alias files from XDG_CONFIG_HOME
    - Move existing aliases from .zshrc to appropriate alias files

---
+++ dot_config/zsh/aliases.zsh
@@ -0,0 +1,34 @@
+# General aliases - Used across all systems
+# These aliases are sourced in .zshrc
+
+# Shell management
+alias reload!='. ~/.zshrc'
+alias cls='clear'
+
+# Common tools
+alias ld='lazydocker'
+alias sshp='ssh -o PubkeyAuthentication=no'
+
+# Directory navigation
+# alias ..='cd ..'
+# alias ...='cd ../..'
+# alias ....='cd ../../..'
+
+# Safety nets
+# alias rm='rm -i'
+# alias cp='cp -i'
+# alias mv='mv -i'
+
+# List aliases
+# alias ll='ls -alF'
+# alias la='ls -A'
+# alias l='ls -CF'
+
+# Git shortcuts (add your own)
+# alias gs='git status'
+# alias ga='git add'
+# alias gc='git commit'
+# alias gp='git push'
+# alias gl='git log --oneline --graph --decorate'
+
+# Add your general aliases below
