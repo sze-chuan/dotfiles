11ae56d4af87feb12309640df229d25716f0b31b
Sze Chuan <sze.chuan@gmail.com>
2025-11-24 23:46:56 +0800

    chore: clean up functions

--- dot_config/zsh/work-functions.zsh
+++ dot_config/zsh/work-functions.zsh
@@ -169,3 +169,11 @@ add-ui-ca() {
     fi
 }
 
+# Get Keycloak password from EdgeOS cluster
+get-kc-pw() {
+    if [ -z "$1" ]; then
+        echo "Usage: get-kc-pw <hostname>"
+        return 1
+    fi
+    ssh root@"$1".edgeos.illumina.com "/usr/local/bin/kubectl get secrets keycloak-creds -o jsonpath='{.data.admin_password}'" | base64 --decode
+}
